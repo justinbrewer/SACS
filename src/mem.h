c9446221f9cf17ac13012ac3b98dc4f7fdab0f0c
Justin Brewer <jzb0012@auburn.edu>
2012-08-30 15:14:15 -0500

    mem: Preliminary memory prototypes
    The internal memory model should not be visible outside the memory module, so
    all structs and typedefs describing the memory model should go in mem.c.

---
+++ src/mem.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdint.h>
+
+int mem_init();
+int mem_cleanup();
+
+int mem_dynamic_alloc(uint32_t size);
+int mem_static_alloc(uint32_t size, uint32_t loc);
+
+uint8_t  mem_read8(uint32_t loc);
+uint16_t mem_read16(uint32_t loc);
+uint32_t mem_read32(uint32_t loc);
+
+int mem_write8(uint32_t loc, uint8_t data);
+int mem_write16(uint32_t loc, uint16_t data);
+int mem_write32(uint32_t loc, uint32_t data);
