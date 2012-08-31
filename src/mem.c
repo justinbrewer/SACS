2d679948a7aad23ce6ae5caf74b5029a4e5428c7
Justin Brewer <jzb0012@auburn.edu>
2012-08-30 21:41:46 -0500

    mem: Node creation and deletion functions, and _dyn_alloc skeleton

--- src/mem.c
+++ src/mem.c
@@ -14,16 +14,57 @@ typedef struct {
   uint8_t* mem;
 } MemoryNode;
 
+MemoryNode* _create_node(uint32_t size, MemoryNode* parent);
+int _delete_node(MemoryNode* node);
+uint32_t _dyn_alloc(MemoryNode* node, uint32_t size);
+
 MemoryNode* root;
 
 int mem_init(){
-  root = (MemoryNode*)malloc(sizeof(MemoryNode));
-  memset(root,0,sizeof(MemoryNode));
-  root->size = 32;
+  root = _create_node(32,0);
   return 0;
 }
 
 int mem_cleanup(){
   //No cleanup should really be necessary
+  return 0;
+}
+
+uint32_t mem_dynamic_alloc(uint32_t size){
+  return _dyn_alloc(root,size);
+}
+
+int mem_static_alloc(uint32_t size, uint32_t loc){}
+
+MemoryNode* _create_node(uint32_t size, MemoryNode* parent){
+  MemoryNode* node = (MemoryNode*)malloc(sizeof(MemoryNode));
+  memset(node,0,sizeof(MemoryNode));
+  node->size = size;
+  node->parent = parent;
+  return node;
+}
+
+int _delete_node(MemoryNode* node){
+  if(node->left != 0){
+    _delete_node(node->left);
+  }
+  if(node->right != 0){
+    _delete_node(node->right);
+  }
+  if(node->mem != 0){
+    free(node->mem);
+  }
+  free(node);
+}
+
+uint32_t _dyn_alloc(MemoryNode* node, uint32_t size){
+  switch(node->status){
+  case FREE:
+    break;
+  case SPLIT:
+    break;
+  case FULL:
+    break;
+  }
   return 0;
 }
