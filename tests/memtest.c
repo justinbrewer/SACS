80b55ecd05c0aca8d1a59d6e961da6a5a460312d
Justin Brewer <jzb0012@auburn.edu>
2012-09-01 16:46:44 -0500

    test: Flesh out memtest a bit

--- tests/memtest.c
+++ tests/memtest.c
@@ -1,7 +1,22 @@
 #include "mem.h"
+#include <stdio.h>
 
 int main(void){
+  printf("Initializing\n");
   mem_init();
+
+  printf("Allocating 1MB\n");
+  uint32_t loc = mem_dynamic_alloc(1<<20);
+  
+  printf("Writing 192342\n");
+  mem_write32(loc,192343);
+
+  printf("Reading...\n");
+  uint32_t result = mem_read32(loc);
+  printf("Read %d\n",result);
+
+  printf("Cleaning up\n");
   mem_cleanup();
+
   return 0;
 }
