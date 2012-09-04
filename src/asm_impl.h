3010466cd410db1a561fa73ccb3ea7a433a8026e
Justin Brewer <jzb0012@auburn.edu>
2012-09-04 03:25:36 -0500

    asm: Interface update - Added entry and label structs
    The assembler will be a linear two-pass system. The first pass will decode
    instructions and data declarations, and also keep track of labels; the
    second pass will resolve all references. Finally, theentries will be written
    out to an asm_binary struct that can be immediately executed or written to a
    file.
    
    Note: the asm_binary struct is not (currently) binary-equivalent to the
    executable format. A specific procedure must be used to transform between
    them.

--- src/asm_impl.h
+++ src/asm_impl.h
@@ -5,11 +5,12 @@
 #include <stdint.h>
 
 typedef enum { ADDRESS, REFERENCE } asm_arg_type;
+typedef enum { INSTR, DATA } asm_entry_type;
 
 struct asm_arg {
   asm_arg_type type;
   union data {
-    uint32_t value;
+    uint32_t address;
     char* reference;
   };
 };
@@ -20,6 +21,22 @@ struct asm_instr {
   struct asm_arg* argv;
 };
 
+struct asm_entry {
+  asm_entry_type type;
+  uint32_t loc;
+  uint32_t size;
+
+  union entry {
+    asm_instr* instr;
+    uint32_t data;
+  };
+};
+
+struct asm_label {
+  uint32_t loc;
+  char* name;
+};
+
 struct asm_binary* _create_binary();
 int _delete_binary(struct asm_binary* bin);
 
@@ -28,5 +45,11 @@ int _delete_instr(struct asm_instr* instr);
 
 struct asm_arg* _create_arg();
 int _delete_arg(struct asm_arg* arg);
+
+struct asm_entry* _create_entry();
+int _delete_entry(struct asm_entry* entry);
+
+struct asm_label* _create_label(uint32_t loc, char* name);
+int _delete_entry(struct asm_label* label);
 
 struct asm_instr* asm_decode_instr(char* instr, int argc, char** argv);
