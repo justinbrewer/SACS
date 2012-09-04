ff28f186ef2ebac9afd6a4843b2f76e459f44120
Justin Brewer <jzb0012@auburn.edu>
2012-09-04 00:10:31 -0500

    asm: asm_binary creation and deletion

--- src/asm.c
+++ src/asm.c
@@ -2,6 +2,7 @@
 #include "asm_impl.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 int asm_init(){return 0;}
 int asm_cleanup(){return 0;}
@@ -14,8 +15,18 @@ int asm_free_binary(struct asm_binary* bin){
   return _delete_binary(bin);
 }
 
-struct asm_binary* _create_binary(){}
-int _delete_binary(struct asm_binary* bin){}
+struct asm_binary* _create_binary(){
+  struct asm_binary* bin = (struct asm_binary*)malloc(sizeof(struct asm_binary));
+  memset(bin,0,sizeof(struct asm_binary));
+  return bin;
+}
+
+int _delete_binary(struct asm_binary* bin){
+  if(bin->binary != 0){
+    free(bin->binary);
+  }
+  free(bin);
+}
 
 struct asm_instr* _create_instr(){}
 int _delete_instr(struct asm_instr* instr){}
