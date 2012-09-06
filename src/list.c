4bc17daf42d85b259c9f7d83bd941ee8b8725a72
Justin Brewer <jzb0012@auburn.edu>
2012-09-05 23:14:56 -0500

    misc: Basic List

---
+++ src/list.c
@@ -0,0 +1,29 @@
+#include "list.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+struct list* list_create(uint32_t size, size_t entry_size){
+  struct list* l = (struct list*)malloc(sizeof(struct list));
+  l->size = size;
+  l->ptr = 0;
+  l->entry_size = entry_size;
+  l->data = malloc(size*entry_size);
+}
+
+int list_delete(struct list* l){
+  free(l->data);
+  free(l);
+}
+
+uint32_t list_add(struct list* l, void* data){
+  if(l->size == l->ptr){
+    //TODO: Expand
+  }
+  memcpy(l->data+(l->ptr*l->entry_size),data,entry_size);
+  return l->ptr++;
+}
+
+void* list_get(struct list* l, uint32_t index){
+  return l->data+(index*l->entry_size);
+}
---
+++ src/list.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+
+struct list {
+  uint32_t size;
+  uint32_t ptr;
+  size_t entry_size;
+  void* data;
+};
+
+struct list* list_create(uint32_t size, size_t entry_size);
+int list_delete(struct list* l);
+uint32_t list_add(struct list* l, void* data);
+void* list_get(struct list* l, uint32_t index);
