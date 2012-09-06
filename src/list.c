2f9e1fcc7b52e3499fe863e69a5bc0744406fcb1
Justin Brewer <jzb0012@auburn.edu>
2012-09-05 23:58:35 -0500

    misc: Just use realloc, so much cleaner

--- src/list.c
+++ src/list.c
@@ -18,12 +18,8 @@ int list_delete(struct list* l){
 
 uint32_t list_add(struct list* l, void* data){
   if(l->size == l->ptr){
-    uint32_t new_size = l->size*1.5;
-    void* new_data = malloc(new_size*l->entry_size);
-    memcpy(new_data,l->data,l->size*l->entry_size);
-    free(l->data);
-    l->size = new_size;
-    l->data = new_data;
+    l->size *= 1.5;
+    l->data = realloc(l->data,l->size*l->entry_size);
   }
   memcpy(l->data+(l->ptr*l->entry_size),data,entry_size);
   return l->ptr++;
