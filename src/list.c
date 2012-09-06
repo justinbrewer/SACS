f6e35a3fbbe78c81d80ba867f1ebac3d3d2c1281
Justin Brewer <jzb0012@auburn.edu>
2012-09-05 23:19:47 -0500

    misc: Finished list implementation

--- src/list.c
+++ src/list.c
@@ -18,7 +18,12 @@ int list_delete(struct list* l){
 
 uint32_t list_add(struct list* l, void* data){
   if(l->size == l->ptr){
-    //TODO: Expand
+    uint32_t new_size = l->size*1.5;
+    void* new_data = malloc(new_size*l->entry_size);
+    memcpy(new_data,l->data,l->size*l->entry_size);
+    free(l->data);
+    l->size = new_size;
+    l->data = new_data;
   }
   memcpy(l->data+(l->ptr*l->entry_size),data,entry_size);
   return l->ptr++;
