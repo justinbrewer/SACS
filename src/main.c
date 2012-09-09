852a6be37a352e7c74e086fd4352dea4adf36484
Justin Brewer <jzb0012@auburn.edu>
2012-09-09 01:01:02 -0500

    main: Load binary into memory at 0x400000

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include "mem.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
 int main(int argc, char** argv){
   if(argc != 1){
@@ -13,8 +15,10 @@ int main(int argc, char** argv){
   struct asm_binary* bin = asm_parse_file(argv[1]);
 
   mem_init();
-
-
+  mem_dynamic_alloc(0x400000,MEM_FAKE_ALLOC|MEM_USE_LOCKED);
+  uint32_t loc = mem_dynamic_alloc(bin->size,MEM_USE_LOCKED);
+  assert(loc == 0x400000);
+  memcpy(mem_translate_addr(loc),bin->binary,bin->size);
 
   mem_cleanup();
   asm_cleanup();
