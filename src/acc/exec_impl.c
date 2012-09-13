2552b41b48af4924fc0cd1ba9915707933309b0f
Justin Brewer <jzb0012@auburn.edu>
2012-09-13 00:41:02 -0500

    acc-exec: Use 32bit fixed-length instructions

--- src/acc/exec_impl.c
+++ src/acc/exec_impl.c
@@ -3,32 +3,27 @@
 #include "mem.h"
 
 int exec_run(uint32_t start, uint32_t text, uint32_t data){
-  uint32_t pc = start, acc, arg;
+  uint32_t pc = start, acc, ir;
 
   while(1){
-    switch(mem_read8(pc++)){
+    ir = mem_read32(pc);
+    pc += 4;
+
+    switch(ir & 0xFF){
     case LOAD:
-      arg = mem_read32(pc);
-      pc += 4;
-      acc = mem_read16(data+arg);
+      acc = mem_read32(data + (ir >> 8));
       break;
 
     case STOR:
-      arg = mem_read32(pc);
-      pc += 4;
-      mem_write16(data+arg,acc);
+      mem_write32(data + (ir >> 8),acc);
       break;
 
     case ADD:
-      arg = mem_read32(pc);
-      pc += 4;
-      acc += mem_read16(data+arg);
+      acc += mem_read32(data + (ir >> 8));
       break;
 
     case MUL:
-      arg = mem_read32(pc);
-      pc += 4;
-      acc *= mem_read16(data+arg);
+      acc *= mem_read32(data + (ir >> 8));
       break;
 
     case END:
