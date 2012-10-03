d979031951c720999536d3e5573530fc56313a4e
Justin Brewer <jzb0012@auburn.edu>
2012-10-02 19:08:52 -0500

    gpr: Getting instructions organized

--- src/gpr/asm_impl.c
+++ src/gpr/asm_impl.c
@@ -1,31 +1,81 @@
 #include "asm.h"
 #include "asm_impl.h"
 #include "gpr/instr.h"
+#include "check.h"
 
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <assert.h>
 
-#define OPERATOR(x,y) if(strcmp(operator,#x) == 0){ instr->opcode = x; instr->argc = y; }
+#define CHECK_ARGC if(instr->argc != argc){printf("Error: %s takes %d arguments\n",operator,instr->argc);exit(EXIT_FAILURE);}
 
 struct asm_instr* asm_decode_instr(char* operator, int argc, char argv[MAX_ARGC][MAX_TOKEN_LEN]){
   struct asm_instr* instr = (struct asm_instr*)malloc(sizeof(struct asm_instr));
 
-       OPERATOR(END,0)
-  else OPERATOR(LA,2)
-  else OPERATOR(LB,3)
-  else OPERATOR(LI,2)
-  else OPERATOR(STO,2)
-  else OPERATOR(B,2)
-  else OPERATOR(BEQZ,2)
-  else OPERATOR(BGE,2)
-  else OPERATOR(BNE,2)
-  else OPERATOR(ADD,2)
-  else OPERATOR(ADDI,2)
-  else OPERATOR(SUBI,2)
-  else OPERATOR(MUL,2)
-  else OPERATOR(SYSCALL,0)
+  if(strcmp(operator,"syscall") == 0){
+    instr->opcode = SYSCALL;
+    instr->argc = 0;
+    CHECK_ARGC;
+  }
+
+  else if(strcmp(operator,"la") == 0){
+    instr->opcode = LA;
+    instr->argc = 2;
+    CHECK_ARGC;
+  }
+
+  else if(strcmp(operator,"lb") == 0){
+    instr->opcode = LB;
+    instr->argc = 2;
+    CHECK_ARGC;
+  }
+
+  else if(strcmp(operator,"li") == 0){
+    instr->opcode = LI;
+    instr->argc = 2;
+    CHECK_ARGC;
+  }
+
+  else if(strcmp(operator,"b") == 0){
+    instr->opcode = B;
+    instr->argc = 1;
+    CHECK_ARGC;
+  }
+
+  else if(strcmp(operator,"beqz") == 0){
+    instr->opcode = BEQZ;
+    instr->argc = 2;
+    CHECK_ARGC;
+  }
+
+  else if(strcmp(operator,"bge") == 0){
+    instr->opcode = BGE;
+    instr->argc = 3;
+    CHECK_ARGC;
+  }
+
+  else if(strcmp(operator,"bne") == 0){
+    instr->opcode = BNE;
+    instr->argc = 3;
+    CHECK_ARGC;
+  }
+
+  else if(strcmp(operator,"addi") == 0){
+    instr->opcode = ADDI;
+    instr->argc = 3;
+    CHECK_ARGC;
+  }
+
+  else if(strcmp(operator,"subi") == 0){
+    instr->opcode = SUBI;
+    instr->argc = 3;
+    CHECK_ARGC;
+  }
+
+  else {
+    printf("Error: Unknown argument \"%s\"\n",operator);
+    exit(EXIT_FAILURE);
+  }
 
   return instr;
 }
