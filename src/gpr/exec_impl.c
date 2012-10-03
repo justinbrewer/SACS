f6aa491d88904e9c2c1ad880281ca544c7217c91
Justin Brewer <jzb0012@auburn.edu>
2012-10-02 22:40:46 -0500

    gpr: This union is needed elsewhere, so move it to the general header
    Also give the various types more appropriate names

--- src/gpr/exec_impl.c
+++ src/gpr/exec_impl.c
@@ -1,8 +1,1 @@
 #include "gpr/instr.h"
-
-union gpr_ir_t {
-  uint32_t u;
-  struct gpr_instr_r r;
-  struct gpr_instr_i i;
-  struct gpr_instr_j j;
-};
--- src/gpr/instr.h
+++ src/gpr/instr.h
@@ -4,9 +4,9 @@ typedef enum { SYSCALL=0x00,
 	       LA=0x10, LB=0x11, LI=0x12,
 	       B=0x20, BEQZ=0x21, BGE=0x22, BNE=0x23,
 	       ADDI=0x30, SUBI=0x31,
-             } gpr_instr;
+             } gpr_op_t;
 
-struct gpr_instr_r {
+struct gpr_instr_r_t {
   unsigned int op    : 6;
   unsigned int rs    : 5;
   unsigned int rt    : 5;
@@ -15,14 +15,21 @@ struct gpr_instr_r {
   unsigned int func  : 6;
 };
 
-struct gpr_instr_i {
+struct gpr_instr_i_t {
   unsigned int op     :  6;
   unsigned int rs     :  5;
   unsigned int rd     :  5;
   unsigned int offset : 16;
 };
 
-struct gpr_instr_j {
+struct gpr_instr_j_t {
   unsigned int op   :  6;
   unsigned int addr : 26;
+};
+
+union gpr_instr_t {
+  uint32_t u;
+  struct gpr_instr_r_t r;
+  struct gpr_instr_i_t i;
+  struct gpr_instr_j_t j;
 };
