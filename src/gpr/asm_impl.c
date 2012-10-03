004c2db962c0169e4397ddc061d61a8a1c149484
Justin Brewer <jzb0012@auburn.edu>
2012-10-03 11:49:27 -0500

    gpr: An acceptable process for translating register names

--- src/gpr/asm_impl.c
+++ src/gpr/asm_impl.c
@@ -135,23 +135,25 @@ uint32_t asm_collapse_instr(struct asm_instr* instr){
 }
 
 uint32_t _register_arg(char name[MAX_TOKEN_LEN]){
-  int offset;
+  int i;
+  static char register_names[32][3] = {"zero","at","v0","v1","a0","a1","a2","a3",
+                                       "t0","t1","t2","t3","t4","t5","t6","t7",
+                                       "s0","s1","s2","s3","s4","s5","s6","s7",
+                                       "t8","t9","k0","k1","gp","sp","fp","ra"};
 
-  if(name[0] != '$'){
-    printf("Error: Expected register, got \"%s\"\n",name);
-    exit(EXIT_FAILURE);
+  if(isnum(name[1])){
+    return atoi(name+1);
   }
 
-  switch(name[1]){
-  case 't':
-    offset = name[2] - 0x30;
-    if(offset > 7){
-      return offset + 16;
+  for(i=0;i<32;i++){
+    if(strcmp(name,register_names[i]) == 0){
+      break;
     }
-    return offset + 8;
-
-  case 's':
-    offset = name[2] - 0x30;
-    return offset + 16;
   }
+  if(i < 32){
+    return i;
+  }
+
+  printf("Error: Unknown register \"%s\"\n",name);
+  exit(EXIT_FAILURE);
 }
