52207a19b8aaf73a39bf079337598a8e234dbf21
Justin Brewer <jzb0012@auburn.edu>
2012-10-03 11:49:39 -0500

    gpr: Make sure it's actually a register name. Also give it an appropriate name

--- src/gpr/asm_impl.c
+++ src/gpr/asm_impl.c
@@ -13,7 +13,7 @@
     exit(EXIT_FAILURE);							\
   }
 
-uint32_t _register_arg(char name[MAX_TOKEN_LEN]);
+uint32_t _translate_reg_name(char name[MAX_TOKEN_LEN]);
 
 struct asm_instr* asm_decode_instr(char* operator, int argc, char argv[MAX_ARGC][MAX_TOKEN_LEN]){
   struct asm_instr* instr = (struct asm_instr*)malloc(sizeof(struct asm_instr));
@@ -134,12 +134,17 @@ uint32_t asm_collapse_instr(struct asm_instr* instr){
   return res.u;
 }
 
-uint32_t _register_arg(char name[MAX_TOKEN_LEN]){
+uint32_t _translate_reg_name(char name[MAX_TOKEN_LEN]){
   int i;
   static char register_names[32][3] = {"zero","at","v0","v1","a0","a1","a2","a3",
                                        "t0","t1","t2","t3","t4","t5","t6","t7",
                                        "s0","s1","s2","s3","s4","s5","s6","s7",
                                        "t8","t9","k0","k1","gp","sp","fp","ra"};
+
+  if(name[0] != '$'){
+    printf("Error: Expected register name, got \"%s\"\n",name);
+    exit(EXIT_FAILURE);
+  }
 
   if(isnum(name[1])){
     return atoi(name+1);
