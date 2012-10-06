b6eaabcf83a626a5aa4cb11db2623bdc0f019d6c
Justin Brewer <jzb0012@auburn.edu>
2012-10-05 23:17:46 -0500

    Memory dump

--- src/main.c
+++ src/main.c
@@ -22,6 +22,28 @@ int main(int argc, char** argv){
 
   exec_run(loc,loc,loc+bin->data_segment);
 
+  //Memory Dump
+  uint8_t* ptr = (uint8_t*)mem_translate_addr(loc);
+  uint32_t i;
+
+  printf("\n=== Memory Dump ===\n.text");
+  for(i=0,loc=0x400000; i < bin->data_segment; i++, ptr++){
+    if(i%8 == 0){
+      printf("\n%#8x: ", loc + i);
+    }
+    printf("%02x ", *ptr);
+  }
+  printf("\n");
+
+  printf("\n.data");
+  for(loc+=i, i=0; i < bin->size; i++, ptr++){
+    if(i%8 == 0){
+      printf("\n%#8x: ", loc + i);
+    }
+    printf("%02x ", *ptr);
+  }
+  printf("\n");
+
   mem_cleanup();
   return 0;
 }
