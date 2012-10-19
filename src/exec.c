b57c4569be2af39c330698fc54d07053028701ee
Justin Brewer <jzb0012@auburn.edu>
2012-10-19 11:32:52 -0500

    exec: State structure, basic implementation of exec_run

--- src/exec.c
+++ src/exec.c
@@ -23,12 +23,39 @@ struct exec_pipe_memwb_t {
   uint32_t alu_out;
 };
 
-void exec_pipe_if(uint32_t pc, struct exec_pipe_ifid_t& out);
-void exec_pipe_id(struct exec_pipe_ifid_t& in, struct exec_pipe_idex_t& out);
-void exec_pipe_ex(struct exec_pipe_idex_t& in, struct exec_pipe_exmem_t& out);
-void exec_pipe_mem(struct exec_pipe_exmem_t& in, struct exec_pipe_memwb_t& out);
-void exec_pipe_wb(struct exec_pipe_memwb_t& in);
+struct exec_state_t {
+  uint32_t running;
+  uint32_t pc;
+  uint32_t text;
+  uint32_t data;
+  uint32_t reg[32];
+  struct exec_pipe_ifid_t if_id;
+  struct exec_pipe_idex_t id_ex;
+  struct exec_pipe_exmem_t ex_mem;
+  struct exec_pipe_memwb_t mem_wb;
+};
+
+void exec_pipe_if(struct exec_state_t& state);
+void exec_pipe_id(struct exec_state_t& state);
+void exec_pipe_ex(struct exec_state_t& state);
+void exec_pipe_mem(struct exec_state_t& state);
+void exec_pipe_wb(struct exec_state_t& state);
 
 int exec_run(uint32_t start, uint32_t text, uint32_t data){
-  
+  struct exec_state_t state = {0};
+  state.running = 1;
+  state.pc = start;
+  state.text = text;
+  state.data = data;
+
+  while(state.running){
+    exec_pipe_wb(state);
+    exec_pipe_mem(state);
+    exec_pipe_ex(state);
+    exec_pipe_id(state);
+    exec_pipe_if(state);
+  }
+
+  return 0;
 }
+
