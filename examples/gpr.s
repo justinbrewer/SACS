d7d4e8239bcf4ac4e0bc1e8332621e9d0244c945
Justin Brewer <jzb0012@auburn.edu>
2012-10-20 13:58:53 -0500

    test: Revert "test: Need some hazard-free testing"
    This reverts commit eeeb226614ae6c73386b7911c9879b1723c1e006.

--- examples/gpr.s
+++ examples/gpr.s
@@ -21,183 +21,55 @@ main:
 	
 ## read the string S:
 	la $a0, string_space
-	nop
-	nop
-	nop
-	nop
-	
 	li $a1, 1024
-	nop
-	nop
-	nop
-	nop
-	
 	li $v0, 8		# load "read_string" code into $v0.
-	nop
-	nop
-	nop
-	nop
-	
 	syscall
-	nop
-	nop
-	nop
-	nop
 
 	la $t1, string_space	# A = S.
-	nop
-	nop
-	nop
-	nop
-	
 	la $t2, string_space	# we need to move B to the end
-	nop
-	nop
-	nop
-	nop
-	
+
 length_loop:			# length of the string
 	lb $t3, ($t2)		# load the byte at addr B into $t3.
-	nop
-	nop
-	nop
-	nop
-	
 	beqz $t3, end_length_loop # if $t3 == 0, branch out of loop.
-	nop
-	nop
-	nop
-	nop
 
 	addi $t2, $t2, 1	# otherwise, increment B,
-	nop
-	nop
-	nop
-	nop
-	
 	b length_loop		# and repeat the loop.
-	nop
-	nop
-	nop
-	nop
 
 end_length_loop:
 	subi $t2, $t2, 2	# subtract 2 to move B back past
-	nop
-	nop
-	nop
-	nop
 
 # the '\0' and '\n'.
 
 test_loop:
 	bge $t1, $t2, is_palin	# if A >= B, it's a palindrome.
-	nop
-	nop
-	nop
-	nop
 
 	lb $t3, ($t1)		# load the byte at addr A into $t3,
-	nop
-	nop
-	nop
-	nop
-	
 	lb $t4, ($t2)		# load the byte at addr B into $t4.
-	nop
-	nop
-	nop
-	nop
-	
 	bne $t3, $t4, not_palin # if $t3 != $t4, not a palindrome.
-	nop
-	nop
-	nop
-	nop
 
 # Otherwise,
 
 	addi $t1, $t1, 1	# increment A,
-	nop
-	nop
-	nop
-	nop
-	
 	subi $t2, $t2, 1	# decrement B,
-	nop
-	nop
-	nop
-	nop
-	
 	b test_loop		# and repeat the loop.
-	nop
-	nop
-	nop
-	nop
 
 is_palin:			# print the is_palin_msg, and exit.
 
 	la $a0, is_palin_msg
-	nop
-	nop
-	nop
-	nop
-	
 	li $v0, 4
-	nop
-	nop
-	nop
-	nop
-	
 	syscall
-	nop
-	nop
-	nop
-	nop
-	
 	b exit
-	nop
-	nop
-	nop
-	nop
 
 not_palin:
 	la $a0, not_palin_msg	## print the not_palin_msg, and exit.
-	nop
-	nop
-	nop
-	nop
-	
 	li $v0, 4
-	nop
-	nop
-	nop
-	nop
-	
 	syscall
-	nop
-	nop
-	nop
-	nop
-	
 	b exit
-	nop
-	nop
-	nop
-	nop
 
 exit:				# exit the program
 	li $v0, 10		# load "exit" code into $v0.
-	nop
-	nop
-	nop
-	nop
-	
 	syscall			# make the system call.
-	nop
-	nop
-	nop
-	nop
+
 
 	.data
 is_palin_msg: .asciiz "The string is a palindrome.\n"
