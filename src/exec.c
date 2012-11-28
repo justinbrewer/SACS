7a0afee92626bab08279dda6a7a490d0f629eec1
Justin Brewer <jzb0012@auburn.edu>
2012-11-27 21:13:49 -0600

    exec: Basic implementation of scoreboard

--- src/exec.c
+++ src/exec.c
@@ -1,27 +1,24 @@
 #include "exec.c"
 
+#include <assert.h>
 #include <stdint.h>
 
 #define NUM_UNITS 4
 
-typedef enum { FALSE=0, TRUE=1 } bool;
-typedef enum { U_NONE=0, U_INT, U_FPADD, U_FPMULT, U_FPDIV } exec_funit_t;
-typedef enum { I_FREE=0, I_ISSUE, I_READ, I_EXEC, I_WRITE } exec_instr_status_t;
+//Raw conversion to float
+#define FLOAT(x) (*(float*)(&x))
 
-union exec_fpr_t {
-  uint32_t i;
-  float f;        //Hopefully 32 bits
-};
+typedef enum { FALSE=0, TRUE=1 } bool;
+typedef enum { U_NONE=-1, U_INT=0, U_FPADD=1, U_FPMULT=2, U_FPDIV=3 } exec_funit_t;
 
 struct exec_funit_state_t {
-  exec_funit_t unit;
-  uint32_t cycle_len;
+  uint32_t cycles;
 
+  unint32_t time;
+  gpr_op_t op;
   uint32_t rd;
   uint32_t rs;
   uint32_t rt;
-  exec_funit_t rs_p;
-  exec_funit_t rt_p;
   bool rs_r;
   bool rt_r;
 };
@@ -33,10 +30,8 @@ struct exec_state_t {
 
   int running;
 
-  uint32_t reg[32];
-  union exec_fpr_t fpr[32];
+  uint32_t reg[64];
 
-  exec_instr_status_t instr_status[NUM_UNITS];
   struct exec_funit_state_t funit_state[NUM_UNITS];
   exec_funit_t reg_status[64];
 
@@ -45,10 +40,11 @@ struct exec_state_t {
 
 void exec_issue(struct exec_state_t* current, struct exec_state_t* next);
 void exec_read(struct exec_state_t* current, struct exec_state_t* next);
-void exec_unit(struct exec_state_t* current, struct exec_state_t* next, uint32_t unit);
+void exec_units(struct exec_state_t* current, struct exec_state_t* next, uint32_t unit);
 void exec_write(struct exec_state_t* current, struct exec_state_t* next);
 
 struct exec_stats_t* exec_run(uint32_t start, uint32_t text, uint32_t data){
+  int i;
   struct exec_state_t current = {0}, next;
 
   current.running = 1;
@@ -56,24 +52,19 @@ struct exec_stats_t* exec_run(uint32_t start, uint32_t text, uint32_t data){
   current.text = text;
   current.data = data;
 
-  current.funit_state[0].unit = U_INT;
-  current.funit_state[0].cycle_len = 1;
+  current.funit_state[U_INT].cycles = 1;
+  current.funit_state[U_FPADD].cycles = 10;
+  current.funit_state[U_FPMUL].cycles = 20;
+  current.funit_state[U_FPDIV].cycles = 40;
 
-  current.funit_state[1].unit = U_FPADD;
-  current.funit_state[1].cycle_len = 10;
-
-  current.funit_state[2].unit = U_FPMULT;
-  current.funit_state[2].cycle_len = 20;
-
-  current.funit_state[3].unit = U_FPDIV;
-  current.funit_state[3].cycle_len = 40;
+  for(i=0;i<64;i++) current.reg_status[i] = U_NONE;
 
   memcpy(&next,&current,sizeof(struct exec_state_t));
 
   while(current.running){
     exec_issue(&current,&next);
     exec_read(&current,&next);
-    for(i=0;i<NUM_UNITS;i++) exec_unit(&current,&next,i);
+    exec_units(&current,&next);
     exec_write(&current,&next);
 
     memcpy(&current,&next,sizeof(struct exec_state_t));
@@ -85,17 +76,113 @@ struct exec_stats_t* exec_run(uint32_t start, uint32_t text, uint32_t data){
 }
 
 void exec_issue(struct exec_state_t* current, struct exec_state_t* next){
-  
+  int rd;
+  exec_funit_t funit;
+  union gpr_instr_t instr = mem_read32(current->pc);
+  next->pc = current->pc + 4;
+
+  switch(instr.j.op){
+  case ADD:
+  case SUB:
+    funit = U_INT;
+    rd = instr.r.rd;
+    break;
+
+  case ADDI:
+  case SUBI:
+    funit = U_INT;
+    rd = instr.i.rd;
+    break;
+  }
+
+  if(current->funit_state[funit]->op == NOP && current->reg_status[rd] == U_NONE){
+    next->funit_state[funit]->time = current->funit_state[funit]->cycles;
+    next->funit_state[funit]->op = instr.j.op;
+    next->reg_status[rd] = funit;
+  } else {
+    next->pc = current->pc;
+    return;
+  }
+
+  switch(instr.j.op){
+  case ADD:
+  case SUB:
+    next->funit_state[funit]->rd = instr.r.rd;
+
+    next->funit_state[funit]->rs = instr.r.rs;
+    next->funit_state[funit]->rs_r = FALSE;
+    
+    next->funit_state[funit]->rt = instr.r.rt;
+    next->funit_state[funit]->rt_r = FALSE;
+    break;
+
+  case ADDI:
+  case SUBI:
+    next->funit_state[funit]->rd = instr.i.rd;
+
+    next->funit_state[funit]->rs = instr.i.rs;
+    next->funit_state[funit]->rs_r = FALSE;
+
+    next->funit_state[funit]->rt = instr.i.offset;
+    next->funit_state[funit]->rt_r = TRUE;
+    break;
+  }
 }
 
 void exec_read(struct exec_state_t* current, struct exec_state_t* next){
-  
+  int i;
+  for(i=0;i<NUM_UNITS;i++){
+    if(current->funit_state[i]->rs_r == FALSE){
+      if(current->reg_status[current->funit_state[i]->rs] == U_NONE){
+	next->funit_state[i]->rs = current->reg[current->funit_state[i]->rs];
+	next->fuint_state[i]->rs_r = TRUE;
+      }
+    }
+
+    if(current->funit_state[i]->rt_r == FALSE){
+      if(current->reg_status[current->funit_state[i]->rt] == U_NONE){
+	next->funit_state[i]->rt = current->reg[current->funit_state[i]->rt];
+	next->fuint_state[i]->rt_r = TRUE;
+      }
+    }
+  }
 }
 
-void exec_unit(struct exec_state_t* current, struct exec_state_t* next, uint32_t unit){
-  
+void exec_units(struct exec_state_t* current, struct exec_state_t* next, uint32_t unit){
+  int i;
+  for(i=0;i<NUM_UNITS;i++){
+    if(current->funit_state[i]->time == 0) continue;
+    if(current->funit_state[i]->rs_r == FALSE) continue;
+    if(current->funit_state[i]->rt_r == FALSE) continue;
+    
+    next->funit_state[i]->time = current->funit_state[i]->time - 1;
+    if(next->funit_state[i]->time > 0) continue;
+    
+    switch(current->funit_state[i]->op){
+    case ADD:
+    case ADDI:
+      rd = rs + rt;
+      break;
+
+    case SUB:
+    case SUBI:
+      rd = rs - rt;
+      break;
+    }
+  }
 }
 
 void exec_write(struct exec_state_t* current, struct exec_state_t* next){
-  
+  int i, rd;
+  for(i=0;i<NUM_UNITS;i++){
+    if(current->funit_state[i]->time == 0){
+      //Replace this with something not stupid (we need more data in funit_state?)
+      for(rd=0;rd<64;rd++) if(current->reg_status[rd] == i) break;
+      assert(rd < 64);
+      
+      next->reg[rd] = current->funit_state[i]->rd;
+      next->reg_status[rd] = U_NONE;
+      return; //Presumably we can only write once per cycle
+    }
+  }
 }
